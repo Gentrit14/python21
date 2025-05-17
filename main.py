7b2a9bf28c335200779400d6f19cbfff41fba4e3
Student <1231@1321>
2025-05-17 03:47:27 -0700

    ...

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-data{
-
+data ={
+    "name": "Johny Deep",
+    "age": 30,
+    "address":{
+        "street": "123 street",
+        "city": "Prishtina"
+    },
+    "contact": [
+        {
+            "type": "email"
+        },
+        {
+            "type": "phone"
+        }
+    ]
 }
+
+print(data["contact"][0]["type"])
